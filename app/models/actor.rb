f0dc9e9bcd5bef51ed3a6b55f5cf15a69455aa89
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-01-28 15:21:25 -0500

    commit tree 82f65f1752c1fb097fdd632f96f86e768162b65c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.shows.collect do |s| 
+      "#{Character.find_by(actor_id: self.id, show_id: s.id).name} - #{s.name}"
+    end
+  end
 end
